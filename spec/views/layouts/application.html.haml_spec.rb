4977eb770f1cb6e12bfa0559b58fa9caf8ae73cd
moeabdol <admin.r99@gmail.com>
2015-10-06 00:52:03 +0300

    modify index view test

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "layouts/application.html.haml", type: :view do
   it "shows correct year in footer" do
     current_year = Time.now.strftime("%Y")
     render
-    expect(rendered).to have_content(current_year)
+    expect(rendered).to have_selector("footer .footer_copyright",
+                                      text: current_year)
   end
 end
